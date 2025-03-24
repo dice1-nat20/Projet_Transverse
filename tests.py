bc0778c417ff9b6ee6614d4fbe21c2250b3fd2b9
Sacha <sacha.bertaina--david@efrei.net>
2025-03-24 14:32:11 +0100

    j'ai mis le code dans le bon fichier

--- tests.py
+++ tests.py
@@ -6,20 +6,3 @@
 ##       Bertaina--David Sacha          ##
 ##       Projet : Transverse            ##
 ##########################################
-import pygame
-from pygame.locals import *
-import pygame, sys
-
-
-background_color = (0,0,255)
-screen = pygame.display.set_mode((500,500))
-pygame.display.set_caption("test-window")
-screen.fill(background_color)
-pygame.display.flip()
-running = True
-
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
