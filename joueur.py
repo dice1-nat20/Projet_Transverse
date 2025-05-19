10b04aa2131a17c91b8633dd83b27285a33595f9
Sacha Bertaina--David <86232952+dice1-nat20@users.noreply.github.com>
2025-05-19 08:27:03 +0200

    petite avancée

--- joueur.py
+++ joueur.py
@@ -6,3 +6,16 @@
 ##       Bertaina--David Sacha          ##
 ##       Projet : Transverse            ##
 ##########################################
+import pygame
+from pygame.locals import *
+import pygame, sys
+
+
+class Player:
+    def __init__(self,sprite):
+        player_img = pygame.image.load(sprite)
+        playerX, playerY = 100,500 #position initiale
+    def drawPlayer(self,window):
+        window.blit(self.player_img,(self.playerX,self.playerY))
+
+
--- windows.py
+++ windows.py
@@ -9,17 +9,27 @@
 import pygame
 from pygame.locals import *
 import pygame, sys
+from joueur import *
 
 
 background_color = (0,0,255)
-screen = pygame.display.set_mode((500,500))
-pygame.display.set_caption("test-window")
-screen.fill(background_color)
-pygame.display.flip()
+width = 1000
+heigh = 750
+playerSprite = 'images&otherFiles/placeHolderPlayer.png'
 running = True
 
+screen = pygame.display.set_mode((width,heigh))
+pygame.display.set_caption("test_window_2")
+screen.fill(background_color)
+pygame.display.flip()
+
 while running:
+    Player.drawPlayer(playerSprite,screen)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
+            sys.exit()
             running = False
+    pygame.display.update()
+pygame.quit()
+
 
