e544b10af7a431370d72dccbacb7c519ec135e4f
feiafeifei <lana.chen@efrei.net>
2025-05-27 08:39:40 +0200

    main

--- ennemis.py
+++ ennemis.py
@@ -13,17 +13,18 @@
 import pygame
 
 class Ennemi:
-    def __init__(self, x, y, width, height, speed, screen_width):
-        self.x = x
+    def __init__(self, x_centre, y, width, height, speed, amplitude):
+        self.x_centre = x_centre
         self.y = y
         self.width = width
         self.height = height
         self.speed = speed
-        self.screen_width = screen_width
+        self.amplitude = amplitude
+        self.x = x_centre - amplitude
 
     def move_ennemi(self):
         self.x += self.speed
-        if self.x <= 0 or self.x + self.width >= self.screen_width:
+        if self.x <= self.x_centre - self.amplitude or self.x + self.width >= self.x_centre + self.amplitude:
             self.speed *= -1
 
     def draw_ennemi(self, surface):
