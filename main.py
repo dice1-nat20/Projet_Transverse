c245a15de2d181c2d6845db67a4d7aec65d15114
Sacha Bertaina--David <86232952+dice1-nat20@users.noreply.github.com>
2025-05-27 11:27:55 +0200

    on avance BIEN !!

--- main.py
+++ main.py
@@ -8,65 +8,12 @@
 ##########################################
 import sys
 
-from ennemis import *
+from Player import *
+from Enemy import *
 from windows import *
 
+
 pygame.init()
-
-# constantes
-PLAYER_SPRITE = pygame.image.load("images&otherFiles/placeHolderPlayer.png")
-PLAYER_X = 50
-PLAYER_Y = 50
-GRAVITE = 1
-FORCE_DU_SAUT = 25
-VITESSE_DEPLACEMENT = 5
-
-
-class Player(pygame.sprite.Sprite):
-    def __init__(self):
-        super().__init__()  # ça créer un autre objet pour le sprite à venir
-        self.image = PLAYER_SPRITE
-        self.rect = self.image.get_rect()  # le rectangle du sprite (sa hitbox)
-        self.rect.x = 100
-        self.rect.y = SCREEN_HEIGHT - 130
-        self.vel_x = 0  # la velocité en x
-        self.vel_y = 0  # la velocité en y
-        self.a = 0
-        self.t = 0
-        self.isJumping = False
-        self.x0 = self.rect.x
-        self.y0 = self.rect.y
-
-    def update(self, keys):
-        if not self.isJumping:
-            # mouvements
-            self.vel_x = 0
-            if keys[pygame.K_LEFT]:
-                self.vel_x = -VITESSE_DEPLACEMENT
-            elif keys[pygame.K_RIGHT]:
-                self.vel_x = VITESSE_DEPLACEMENT
-            self.rect.x += self.vel_x
-
-            # saut
-            if keys[pygame.K_SPACE] and not self.isJumping:
-                self.vel_y = -FORCE_DU_SAUT
-                self.isJumping = True
-                self.t = 0
-                self.x0 = self.rect.x
-                self.y0 = self.rect.y
-        else:
-            self.t += 1  # / FPS
-            newX = self.vel_x * self.t
-            newY = self.vel_y * self.t + 0.5 * GRAVITE * (self.t ** 2)
-            self.rect.x = self.x0 + newX
-            self.rect.y = self.y0 + newY
-
-            if self.rect.bottom >= SCREEN_HEIGHT:
-                self.rect.bottom = SCREEN_HEIGHT
-                self.isJumping = False
-                self.vel_x = 0
-                self.vel_y = 0
-
 
 player = Player()
 player_group = pygame.sprite.Group()
@@ -87,7 +34,6 @@ while running:
             running = False
 
     keys = pygame.key.get_pressed()
-
     player.update(keys)
     enemies.update()
 
@@ -102,3 +48,8 @@ while running:
 
 pygame.quit()
 sys.exit()
+
+# en gros, on a besoni de transformer FORCE_DU_SAUT et VITESSE_DEPLACEMENT en variables pour pouvoir utilisé les equations horraires.
+# avec vitesse_deplacement (en variable), on aurait de l'inertie, et force_du_saut (en variable aussi) dépendrait de vitesse_deplacement
+# on pourrait ensuite ajouter une touche (genre fleche haut et bas) qui modifierait la gravité, QUE si isJumping == False !!
+# après pour le niveau il faudra modifier le groundLvL pour faire genre c'est une plateformes (alors que ce sera totalement une image)
--- tests.py
+++ tests.py
@@ -16,29 +16,3 @@ from windows import screen
 from time import sleep
 
 
-playerSprite = 'images&otherFiles/placeHolderPlayer.png'
-running = True
-
-while running:
-    x = 250
-    y = 500
-
-    joueur = Player(playerSprite,x,y,80)
-    screen.blit(joueur.playerSpr,(x,y))
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit()
-            running = False
-        elif event.type == pygame.K_SPACE:
-            pos = joueur.playerJump()
-            for i in pos:
-                joueur.playerX = i[0]
-                joueur.playerY = i[1]
-                joueur.drawPlayer(joueur.playerX,joueur.playerY)
-                pygame.display.update()
-                pygame.display.flip()
-                sleep(10)
-                i += 1
-    pygame.display.update()
-
-pygame.quit()
