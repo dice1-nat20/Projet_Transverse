928a07e5ecf1df0fb26006ab330d3e042a6568e3
Sacha Bertaina--David <86232952+dice1-nat20@users.noreply.github.com>
2025-06-01 16:15:34 +0200

    courbe pendant le sauts : fait

--- windows.py
+++ windows.py
@@ -14,23 +14,27 @@ from constantes import SCREEN_HEIGHT, SCREEN_WIDTH, backgroundIMG
 
 
 # fenêtre
-worldShift = 0
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Monic")
 clock = pygame.time.Clock()
 
-def drawWindow(player,enemies,plateformes, backgroundX):
-    backgroundW = backgroundIMG.get_width()
-    tiles = (SCREEN_WIDTH//backgroundW)+2
+def drawWindow(player,enemies,plateformes, backgroundX,scroll,pointsDeLaTrajectoire):
+    if backgroundIMG:
+        backgroundLargeur = backgroundIMG.get_width()
+        tiles = (SCREEN_WIDTH//backgroundLargeur)+2
+        for i in range(tiles):
+            screen.blit(backgroundIMG,(i*backgroundLargeur-(scroll % backgroundLargeur),0))
+    else:
+        screen.file((255,255,255))
 
-    for i in range(tiles):
-        screen.blit(backgroundIMG,(i*backgroundW-(backgroundX%backgroundW),0))
+    for x,y in pointsDeLaTrajectoire:
+        pygame.draw.circle(screen,(0,255,0),(x - scroll,y),3)
 
     for plt in plateformes:
-        pygame.draw.rect(screen,(100,100,100),(plt['x'],plt['y'],plt['width'],10))
+        pygame.draw.rect(screen,(100,100,100),(plt['x']-scroll,plt['y'],plt['width'],10))
 
     for enemi in enemies:
-        screen.blit(enemi.image, (enemi.rect.x, enemi.rect.y))
+        screen.blit(enemi.image, (enemi.rect.x - scroll, enemi.rect.y))
 
     screen.blit(player.image,(player.rect.x,player.rect.y))
     pygame.display.flip()
