b8b13e7f2f9325bbf51daa59201632e6336a6a57
Sacha Bertaina--David <86232952+dice1-nat20@users.noreply.github.com>
2025-06-02 11:18:17 +0200

    .

--- constantes.py
+++ constantes.py
@@ -7,7 +7,7 @@ FPS = 60
 backgroundIMG = pygame.image.load("images&otherFiles/jk.png")
 GREEN = (0, 255, 0)
 VITESSE_DE_SCROLL = 2.5
-VICTORY_ZONE = pygame.Rect(2000,0, 100, SCREEN_HEIGHT)
+VICTORY_ZONE = pygame.Rect(3000,0, 100, SCREEN_HEIGHT)
 
 # sprites
 PLAYER_SPRITE = pygame.image.load("images&otherFiles/placeHolderPlayer.png")
